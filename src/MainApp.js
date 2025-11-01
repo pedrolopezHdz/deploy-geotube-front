eb7479362e9b4d4838952aa1cf9664a02b0a1313
pedrolopezHdz <21160689@itoaxaca.edu.mx>
2025-11-01 12:42:04 -0600

    probando reducir buscador

--- src/MainApp.js
+++ src/MainApp.js
@@ -1020,7 +1020,7 @@ const MainApp = () => {
                   value={searchTerm}
                   onChange={handleSearchChange}
                   onFocus={handleSearchFocus}
-                  className="search-input glass-effect bg-gray-800 border border-gray-700 rounded-lg px-4 py-2 text-white focus:outline-none focus:ring-2 focus:ring-cyan-500 w-80 pr-10"
+                  className="search-input glass-effect bg-gray-800 border border-gray-700 rounded-lg px-4 py-2 text-white focus:outline-none focus:ring-2 focus:ring-cyan-500 w-60 pr-10"
                 />
                 <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
                   <svg className="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
